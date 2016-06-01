8cafdf2d2fc19d46ac29da2b491921829c757dd4
Jacob Francis Powers <xxv9xx@gmail.com>
2016-05-31 22:09:43 -0500

    Deap property and self-resolving element queries

--- utemplate.js
+++ utemplate.js
@@ -9,62 +9,112 @@ Template = (function(element) {
 
     constructor.className = "Template";
 
+    function asElement(element) {
+
+      // find specified element
+      if (element.constructor === String) {
+        element = document.querySelector(element);
+      }
+
+      // it's a jQuery node
+      if (element.constructor.name === 'oe') {
+        element = element.get(0);
+      }
+
+      if (element instanceof Element && element.tagName === 'TEMPLATE') {
+        element = element.content;
+      }
+
+      if (element instanceof DocumentFragment) {
+        element = element.firstElementChild;
+      }
+
+      console.log(element);
+
+      // element is already provided
+      if (element instanceof Element) {
+        return element;
+      } else {
+        throw new Error("An invalid or unknown document element node specified.");
+      }
+    }
+    function query(object, property) {
+      // original featured on gde33@freenode#javascript
+      // https://jsfiddle.net/gaby_de_wilde/cthj49aw/4/
+
+      var o;
+      var q;
+
+      o = object;
+      q = property;
+
+      keyset = (q.split(/[.{}]/g)).filter(function(e){return e});
+      result = keyset.reduce(function(oo,nn){ return oo[nn]},o);
+
+      return result;
+    }
     prototype.setElement = function(element) {
-        element = element.content.firstElementChild;
+        element = asElement(element);
 
         this.element = element;
 
         return element;
     };
 
+    prototype.configureAddons = function() {
+      this.mods = this.mods || {};
+      this.mods.time = this.mods.time || {};
+      this.mods.time.iso = function() {
+        return (new Date()).toISOString();
+      };
+    };
+
     prototype.prepare = function(data) {
-        var element, html, mods;
+        var element, html, properties;
 
         element = this.element.cloneNode(true);
-
         html = element.innerHTML;
+        properties = /\{([^}]+)\}/g;
 
-        mods = {
-            time: {
-                iso: function() {
-                    return (new Date()).toISOString();
-                }
-            }
-        };
 
-        html = html.replace(/\{([^}]+)\}/g, function() {
-            var original, capture;
-
-            original = arguments[0];
-            capture = arguments[1];
-
-            if (capture.indexOf(":") !== -1) {
-                    var mod, func;
-
-                    mod = capture.split(":");
-                    func = mod.pop();
-                    mod = mod.shift();
-
-                    if (mods[mod] && mods[mod][func]) {
-                        return mods[mod][func](capture);
-                    }
-            } else if (data.hasOwnProperty(capture)) {
-                return data[capture];
-
-            }
-            return original;
-        });
+        html = html.replace(properties, withValues);
 
         element.innerHTML = html;
 
         return element;
+
+        function runAddon(capture) {
+          var mod, func;
+
+          mod = capture.split(":");
+          func = mod.pop();
+          mod = mod.shift();
+
+          if (mods[mod] && mods[mod][func]) {
+              return mods[mod][func](capture);
+          }
+        }
+
+        function withValues(original, capture) {
+          var module;
+          var cache;
+
+          if (capture.indexOf(":") !== -1) {
+            module = runAddon(capture);
+
+            if (module !== null && module !== undefined) {
+              return module;
+            }
+          }
+
+          return query(data, original) || original;
+        }
     };
 
     function Template(element) {
-        this.setElement(element);
+      this.configureAddons();
+      this.setElement(element);
     }
 
     return Template;
 }());
-
-module.exports = Template;
