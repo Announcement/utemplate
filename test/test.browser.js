dd6cc4f47978354a3a6ccedd382f5a11fe93798e
Jacob Francis Powers <xxv9xx@gmail.com>
2016-08-12 15:51:26 -0500

    Add some browser friendly tests

--- test/test.browser.js
+++ test/test.browser.js
@@ -1,33 +1,4 @@
-// import { version } from '../package.json';
-// import { expect } from 'chai';
-// import DOMPlugin from 'chai-dom';
-// import Template from '../index.js';
-var JSONReader;
-
-JSONReader = (function(){
-	var fs = require('fs');
-	var path = require('path');
-	function JSONReader(filename) {
-		filename = path.resolve(__dirname + '/' + filename);
-		var contents;
-		contents = fs.readFileSync(filename);
-		contents = contents.toString();
-		contents = JSON.parse(contents);
-		return contents;
-	}
-	return JSONReader;
-}());
-
-var version = (new JSONReader('../package.json')).version;
-
-var chai = require('chai');
 var expect = chai.expect;
-
-// var DOMPlugin = require('chai-dom');
-
-// var Template = require('../index.js');
-
-// chai.use(DOMPlugin);
 
 describe('Alchemist', function() {
 	describe('@asElement()', function(){});
@@ -61,5 +32,19 @@ describe('Template', function() {
 });
 
 describe('Environment', function() {
-	describe('Template', function(){});
+	describe('Template', function(){
+		it('should be available globally', function() {
+			expect(Template).to.exist;
+		});
+	});
+	describe('Alchemist', function(){
+		it('should be available globally', function() {
+			expect(Alchemist).to.exist;
+		});
+	});
+	describe('Parasite', function(){
+		it('should be available globally', function() {
+			expect(Parasite).to.exist;
+		});
+	});
 });
